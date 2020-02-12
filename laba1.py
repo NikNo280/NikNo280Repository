d138ae204c4d6f8fe4d12bf08ea2241153044977
NikNo280 <60934033+NikNo280@users.noreply.github.com>
2020-02-12 08:44:28 +0300

    Jobe

--- laba1.py
+++ laba1.py
@@ -1,5 +1,12 @@
+i = 7
 with open("Text.txt", "r", encoding='utf-8') as file:
     line = file.readline()
+    d = dict.fromkeys(line, i)
+    i += 1
     while line:
+        other = {i: line}
+        d.update(other)
+        i = i + 1
         print(line, end="")
         line = file.readline()
+print(d.values())
