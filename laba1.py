ba1e1fcebf2b8fbc01bd374bd37b6fad18443a45
NikNo280 <60934033+NikNo280@users.noreply.github.com>
2020-02-12 14:07:30 +0300

    1.1

--- laba1.py
+++ laba1.py
@@ -1,3 +1,4 @@
+
 i = 7
 with open("Text.txt", "r", encoding='utf-8') as file:
     TextStr = file.read()
@@ -7,4 +8,17 @@ with open("Text.txt", "r", encoding='utf-8') as file:
         if((i <= "я" and i >= "а") or (i == " ") or (i <= "z" and i >= "a")):
             UpdatedTextStr += i
     ArrStr = UpdatedTextStr.split(" ")
-    print(ArrStr)
+    Key = 0
+    Count = 0
+    Dictionary = dict.fromkeys(['a', 'b'])
+    Dictionary.clear()
+    for i in ArrStr:
+        for k in ArrStr:
+            if(i == k):
+                Key += 1
+        other = {[Key, Count] : i}
+        Dictionary.update(other)
+        Key = 0
+    print(Dictionary.keys())
+    print(Dictionary.setdefault(1))
+    print(Dictionary.keys())
