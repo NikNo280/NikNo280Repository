6b60c33821d0fe1bb5cc1f4551bba80cfcf6dc8a
NikNo280 <60934033+NikNo280@users.noreply.github.com>
2020-02-12 13:01:36 +0300

    Jobe

--- laba1.py
+++ laba1.py
@@ -1,12 +1,9 @@
 i = 7
 with open("Text.txt", "r", encoding='utf-8') as file:
-    line = file.readline()
-    d = dict.fromkeys(line, i)
-    i += 1
-    while line:
-        other = {i: line}
-        d.update(other)
-        i = i + 1
-        print(line, end="")
-        line = file.readline()
-print(d.values())
+    TextStr = file.read()
+    TextStr = TextStr.lower()
+    UpdatedTextStr = ""
+    for i in TextStr:
+        if((i <= 'a' and i >= 'z') or (i <= 'а' and i >= 'я') or (i == ' ')):
+            print(i)
+    print(UpdatedTextStr)
