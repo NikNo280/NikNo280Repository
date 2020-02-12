9ae4aebd39137d080e242e2c3dbb9d361fbdd069
NikNo280 <60934033+NikNo280@users.noreply.github.com>
2020-02-12 08:33:18 +0300

    Next

--- laba1.py
+++ laba1.py
@@ -1,5 +1,5 @@
-with open("Text.txt", "r") as file:
-    str1 = file.readline()
-    print(str1, end="")
-    str2 = file.readline()
-    print(str2)
+with open("Text.txt", "r", encoding='utf-8') as file:
+    line = file.readline()
+    while line:
+        print(line, end="")
+        line = file.readline()
