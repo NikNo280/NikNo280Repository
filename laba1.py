04a84721ea187de07ddd9fa121a6cd9f99404191
NikNo280 <60934033+NikNo280@users.noreply.github.com>
2020-02-12 17:37:51 +0300

    1.2

--- laba1.py
+++ laba1.py
@@ -19,9 +19,6 @@ with open("Text.txt", "r", encoding='utf-8') as file:
             other = {Key: i}
             Dictionary.update(other)
             Key = 0
-    print(Dictionary)
-    print(Dictionary.setdefault(10))
-    for i in range(1, 10):
+    for i in range(0, 10):
         max_values = max(Dictionary.keys())
-
         print(Dictionary.pop(max_values))
